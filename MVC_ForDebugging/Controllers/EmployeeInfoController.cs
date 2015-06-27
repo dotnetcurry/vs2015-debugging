17668fa3cf7260bc72a8c3fa8271da9bdc025b36
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2015-06-27 22:37:50 +0530

    project added

---
+++ MVC_ForDebugging/Controllers/EmployeeInfoController.cs
@@ -0,0 +1,117 @@
+﻿using System.Web.Mvc;
+
+using MVC_ForDebugging.Models;
+using MVC_ForDebugging.DataAccess;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using System.Linq;
+
+using System.Web.Script.Serialization;
+
+namespace MVC_ForDebugging.Controllers
+{
+    public class EmployeeInfoController : Controller
+    {
+
+        EmployeeDAL obj;
+        public EmployeeInfoController()
+        {
+            obj = new EmployeeDAL(); 
+        }
+
+
+        // GET: EmployeeInfo
+        public ActionResult Index()
+        {
+            var Emps = obj.GetEmployees();
+            int i = 0;
+            var jObj = new JavaScriptSerializer();
+            var jsonData =   jObj.Serialize(Emps);
+            return View(Emps);
+        }
+
+         
+
+        // GET: EmployeeInfo/Details/5
+        public ActionResult Details(int id)
+        {
+            var Emp = obj.GetEmployee(id);
+            return View(Emp);
+        }
+
+        // GET: EmployeeInfo/Create
+        public ActionResult Create()
+        {
+            var Emp = new EmployeeInfo();
+            return View(Emp);
+        }
+
+        // POST: EmployeeInfo/Create
+        [HttpPost]
+        public ActionResult Create(EmployeeInfo Emp)
+        {
+            try
+            {
+                if (obj.CheckEmpNameExist(Emp.EmpName) == true && obj.CheckValidSal(Emp.Salary))
+                {
+                    obj.AddNewEmployee(Emp);
+                    return RedirectToAction("Index");
+                }
+                else {
+                    return View(Emp);
+
+                }
+            }
+            catch
+            {
+                return View(Emp);
+            }
+        }
+
+        // GET: EmployeeInfo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var Emp = obj.GetEmployee(id);
+            return View(Emp);
+        }
+
+        // POST: EmployeeInfo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, EmployeeInfo Emp)
+        {
+            try
+            {
+                obj.UpdateEmployee(id, Emp);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeInfo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var Emp = obj.GetEmployee(id);
+            return View(Emp);
+        }
+
+        // POST: EmployeeInfo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, EmployeeInfo Emp)
+        {
+            try
+            {
+                obj.DeleteEmployee(id, Emp);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(Emp);
+            }
+        }
+    }
+}
---
+++ MVC_ForDebugging/DataAccess/EmployeeDAL.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MVC_ForDebugging.Models;
+namespace MVC_ForDebugging.DataAccess
+{
+    public class EmployeeDAL
+    {
+        ApplicationDBEntities ctx;
+        public EmployeeDAL()
+        {
+            ctx = new ApplicationDBEntities(); 
+        }
+
+        public List<EmployeeInfo> GetEmployees()
+        {
+            return ctx.EmployeeInfoes.ToList();
+        }
+
+        public EmployeeInfo GetEmployee(int id)
+        {
+            return ctx.EmployeeInfoes.Find(id);
+        }
+
+        public void AddNewEmployee(EmployeeInfo Emp)
+        {
+            ctx.EmployeeInfoes.Add(Emp);
+            ctx.SaveChanges();  
+        }
+        public void UpdateEmployee(int id, EmployeeInfo Emp)
+        {
+            var e = ctx.EmployeeInfoes.Find(id);
+            if (e != null)
+            {
+                e.EmpName = Emp.EmpName;
+                e.Salary = Emp.Salary;
+                e.DeptName = Emp.DeptName;
+                e.Designation = Emp.Designation;
+
+                ctx.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Method to Check of the EmpName is already Exist
+        /// </summary>
+        /// <param name="ename"></param>
+        /// <returns></returns>
+
+        public bool CheckEmpNameExist(string ename)
+        {
+            bool isExist = false;
+
+            foreach (var item in ctx.EmployeeInfoes.ToList())
+            {
+                if (item.EmpName.Trim() == ename.ToUpper().Trim())
+                {
+                    isExist = true;
+                    break;
+                }
+            }
+            return isExist;
+        }
+
+        /// <summary>
+        /// Check whethar the Manage has salary greater than 5000
+        /// </summary>
+        /// <param name="desig"></param>
+        /// <param name="sal"></param>
+        /// <returns></returns>
+        public bool CheckValidSal(int sal)
+        {
+            bool isValid = true;
+            if (sal < 5000)
+            {
+                isValid = false;
+            }
+            return isValid;
+        }
+
+
+        public void DeleteEmployee(int id,EmployeeInfo Emp)
+        {
+            var e = ctx.EmployeeInfoes.Find(id);
+            if (e != null)
+            {
+                ctx.EmployeeInfoes.Remove(e);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
